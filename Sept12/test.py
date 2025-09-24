47dc1fc603e1fc4cf2fca3570b50e55f1bb5dae6
maankhedr <maan.khedr@ucalgary.ca>
2025-09-24 14:01:31 -0600

    folder name changes and adding plotting examples

--- Sept12/test.py
+++
@@ -1,8 +0,0 @@
-fileh = open('textdata.txt', 'r')
-print(fileh.read())
-
-import numpy as np
-
-file = np.genfromtxt('en_climate_hourly_AB_3031094_09-2023_P1H.csv', delimiter=',', names=True,dtype='str')
-
-print(file)
